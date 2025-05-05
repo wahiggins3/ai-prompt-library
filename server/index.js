83d0a8e4b21d773eeda11e9fbe094b08ab956f23
William Higgins <wahiggins3@The-Scopuli.local>
2025-05-05 16:30:54 -0400

    Fix import path in server/index.js: - Update db.js import path to be relative to server directory

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { OpenAI } = require('openai');
-const { query, pool } = require('./server/db.js');
+const { query, pool } = require('./db.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
